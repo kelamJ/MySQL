7ec116e4e107cfcbb8b5f778854e26670b27dd6b
kelamJ <malekjulien80@gmail.com>
2022-12-09 12:20:10 +0100

    update

--- Exercice SQL/exercice.sql
+++ Exercice SQL/exercice.sql
@@ -1,4 +1,5 @@
 -- Exercice requête SQL
+-- Lot 1
 -- 1.
 SELECT hot_nom, hot_ville FROM `hotel`; 
 -- 2.
@@ -11,6 +12,7 @@ SELECT cha_numero, cha_capacite FROM `chambre` WHERE cha_capacite > 1;
 SELECT cli_nom, cli_ville FROM `client` WHERE cli_ville != 'Londres'; 
 -- 6.
 SELECT hot_nom, hot_ville, hot_categorie FROM `hotel` WHERE hot_ville = 'Bretou' AND hot_categorie > 3; 
+-- Lot 2
 -- 7.
 SELECT sta_nom, hot_nom, hot_categorie, hot_ville FROM `hotel` INNER JOIN `station` ON hot_sta_id = sta_id; 
 -- 8.
@@ -22,4 +24,6 @@ SELECT cli_nom, hot_nom, res_date FROM `client` JOIN `hotel` ON hotel.hot_id = c
 -- 11.
 SELECT sta_nom, hot_nom, cha_numero, cha_capacite FROM `chambre` JOIN `hotel` ON hotel.hot_id = chambre.cha_id JOIN `station` ON station.sta_id = chambre.cha_id; 
 -- 12.
-SELECT cli_nom, hot_nom, res_date_debut, res_date_fin FROM `client` JOIN `hotel` ON hotel.hot_id = client.cli_id JOIN `reservation` ON reservation.res_id = client.cli_id; 
+SELECT cli_nom, hot_nom, DATEDIFF(res_date_fin, res_date_debut), res_date_debut, res_date_fin FROM `client` JOIN `hotel` ON hotel.hot_id = client.cli_id JOIN `reservation` ON reservation.res_id = client.cli_id;
+-- Lot 3
+-- 13.
